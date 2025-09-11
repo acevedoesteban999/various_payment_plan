ce136dd44e0798e82e7a5ff076fec17add83b559
Esteban Acevedo <acevedoesteban999@gmail.com>
2025-09-11 10:38:24 -0400

    first commit

---
+++ plan_pagos_varios/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Plan Pagos Varios',
+    'version': '',
+    'summary': """ Plan_pagos_varios Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
