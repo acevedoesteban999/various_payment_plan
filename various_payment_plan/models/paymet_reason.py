8848e2ee769868d49702bd80ad03b3f0fe28c145
Esteban Acevedo <acevedoesteban999@gmail.com>
2025-09-11 14:52:11 -0400

    [ADD] vpp: Add method to create or edit the accounting entry account

--- various_payment_plan/models/paymet_reason.py
+++ various_payment_plan/models/paymet_reason.py
@@ -7,6 +7,13 @@ class PaymentReason(models.Model):
 
     active = fields.Boolean(string='Active', default=True)
     name = fields.Char(string='Name', required=True)
-    account_id = fields.Many2one('account.account', string='Account', required=True)
+    account_id = fields.Many2one(
+                    'account.account', 
+                    string='Account', 
+                    required=True,
+                    help="""Account to use for this payment reason.
+                        Changing this option does not edit existing accounting entries, 
+                        it only acts on new ones."""
+                    )
     company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
 
